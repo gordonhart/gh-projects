84f854fa274c1984f4b63b6a9739b5cea6f2d9bb
gordonhart <gordon.hart2@gmail.com>
2015-07-28 02:59:57 -0400

    Gallery view upgrade

--- static/js/gallery_sidebar.js
+++ static/js/gallery_sidebar.js
@@ -1,8 +1,7 @@
-function construct_gallery(){
+window.onload = function(){
 	/*
 	This function populates the sidebar accompanying the gallery.
 	*/
-	console.log("construct called");
 
 	var gallery_main = document.getElementById("gallery-main"); // fixed-position div covering full screen
 	var gallery_side = document.getElementById("gallery-side");
@@ -10,47 +9,84 @@ function construct_gallery(){
 	var items = gallery_main.children;
 
 	for(var i=0; i<items.length; i++){
-		var temp = items[i].cloneNode(true);
+		var new_node = cloneAndScale(items[i]);
+		gallery_side.appendChild(new_node);
+	}
 
-		temp.removeAttribute("id");
-		temp.removeAttribute("href");
-		temp.removeAttribute("onclick");
+	//JQUERY ELEMENTs for SLIDER
+//	document.getElementById('gallery-hover').addEventListener('mousedown', mouseDown, false);
+//	window.addEventListener('mouseup', mouseUp, false);
 
-		temp.style.cursor = "pointer";
+	document.getElementById("gallery-main").addEventListener('scroll', followMain); // add scroll listener to main div
+	document.getElementById("gallery-side").addEventListener('scroll', followSide); // add scroll listener to side div
+};
 
-/*		var fontsize = temp.style.fontSize;
-		console.log("font size is: " + fontsize);
+function cloneAndScale(el){
+	var node = el.cloneNode(true);
 
-		temp.style.fontSize = '0.1rem';
-*/
-		gallery_side.appendChild(temp);
+	node.removeAttribute("id");
+	node.removeAttribute("href");
+	node.removeAttribute("onclick");
+
+	node.classList.add("gallery-side-el");
+
+	var node_children = node.querySelectorAll("*"); // get all child nodes
+	for(var i=0; i<node_children.length; i++){ // apply changes to all child nodes as well
+		node_children[i].removeAttribute("id");
+		node_children[i].removeAttribute("href");
+		node_children[i].removeAttribute("onclick");
+
+	 	node_children[i].classList.add("gallery-side-el");
+	}
+
+	return node;
+}
+
+function followMain(){
+	if($("#gallery-main").is(':hover')){ // only do if hovered to get rid of echo/feedback between the two
+		var main_height = $("#gallery-main")[0].scrollHeight; // this gets the total height of div
+		var side_height = $("#gallery-side")[0].scrollHeight;
+
+		var main_position = $("#gallery-main").scrollTop();
+
+		var ratio = side_height/main_height;
+
+		$("#gallery-side").scrollTop(ratio*main_position);
 	}
 }
 
-/*
-TESTING JQUERY FOR SLIDING PIECE
-*/
+function followSide(){
+	if($("#gallery-side").is(':hover')){
+		var main_height = $("#gallery-main")[0].scrollHeight; // this gets the total height of div
+		var side_height = $("#gallery-side")[0].scrollHeight;
 
-window.onload = addListeners;
+		var side_position = $("#gallery-side").scrollTop();
 
-function addListeners(){
-    document.getElementById('dxy').addEventListener('mousedown', mouseDown, false);
-    window.addEventListener('mouseup', mouseUp, false);
+		var ratio = main_height/side_height;
 
+		$("#gallery-main").scrollTop(ratio*side_position);
+	}
 }
 
-function mouseUp()
-{
-    window.removeEventListener('mousemove', divMove, true);
+/*******************************
+TESTING JQUERY FOR SLIDING PIECE
+*******************************/
+/*
+
+function mouseUp(){
+	window.removeEventListener('mousemove', divMove, true);
 }
 
 function mouseDown(e){
-  window.addEventListener('mousemove', divMove, true);
+	window.addEventListener('mousemove', divMove, true);
 }
 
 function divMove(e){
-    var div = document.getElementById('dxy');
-  div.style.position = 'absolute';
-  div.style.top = e.clientY + 'px';
-  div.style.left = e.clientX + 'px';
-}​
+	var div = document.getElementById('gallery-hover');
+
+	if(e.clientY >= 0 && e.clientY <= (window.innerHeight-100)){
+		div.style.top = e.clientY + 'px';
+	}
+}
+
+*/
